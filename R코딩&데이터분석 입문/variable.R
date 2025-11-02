c493c9b5972417923504dbe9d2c8b218a6425a15
April <ghwon4640@gmail.com>
2025-11-02 19:47:26 +0900

    변수 공부

---
+++ R코딩&데이터분석 입문/variable.R
@@ -0,0 +1,57 @@
+# 변수 만들기
+a <- 10
+
+total <- 5050
+total
+print(total)
+cat('합계 : ',total)
+age <- 20
+cat('내 나이 아마도 ',age)
+
+# 변수를 이용한 산술연산
+a <- 10
+b <- 20
+
+# 변수와 변수 사이의 연산은 변수에 저장된 값들의 연산으로 바꿔서 실행행
+c <- a+b
+print(a)
+print(b)
+print(c)
+
+year_born <- 2006
+current_year <- 2025
+age <- current_year - year_born + 1
+print(age)
+
+# 변수는 변할 수 있느 값이다
+# 하나의 변수에는 하나의 값만 !
+a <- 10
+print(a)
+a <- 50
+print(a)
+
+input <- 13; output <- 29; time <- 5
+input <- input * time # 65
+print(input)
+input <- input-output # 36
+print(input)
+
+# 숫자형과 문자형
+age.1 <- 20
+age.2 <- 25
+print(age.1+age.2)
+name.1 <- 'John'
+print(name.1)
+arade.1 <- '3'
+
+# 논리형
+5>3
+2>4
+TRUE + TRUE # 산술연산에서 TRUE는 1
+a <- TRUE
+b <- F
+a
+b
+a+b
+addr <- NULL
+print(addr)
