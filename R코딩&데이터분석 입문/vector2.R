ca9f9be6cae53014fc441239f53e6e3741105bf8
April <ghwon4640@gmail.com>
2025-11-11 00:54:29 +0900

    벡터의 연산 공부

--- R코딩&데이터분석 입문/vector2.R
+++ R코딩&데이터분석 입문/vector2.R
@@ -51,7 +51,7 @@ length((d))
 mean(d)
 max(d)
 min(d)
-sort(d) # 오름차순순
+sort(d) # 오름차순
 sort(d,decreasing = FALSE) # 오름차순
 sort(d,decreasing = TRUE) # 내림차순
 
@@ -81,3 +81,58 @@ mean(a)
 # 벡터 a의 분산과 표준편차
 var(a)
 sd(a)
+
+# 벡터에 논리연산자 적용
+d <- 1:9
+d
+d >= 5 # d원소 각각이 >=5 인지 검사
+d[d>5] # 6,7,8,9
+sum(d>5) # 5보다 큰 개수를 출력
+sum(d[d>5]) # 5보다 큰 값의 합계 출력
+d == 5
+
+condi <- d > 5 & d < 8 # 조건을 변수에 저장
+d[condi] # 조건에 맞는 값들을 선택
+
+
+# 확인문제 
+a <- 30:120
+
+# 벡터 a에 저장된 값이 3의 배수인지를 비교하는 조건문 condi를 만드세요.
+condi <- a %% 3 == 0
+
+# condi를 이용해 a안에 3의 배수가 몇 개 존재하는지 구하세요.
+sum(condi)
+
+# condi를 이용해 a에서 3의 배수만 추출하세요.
+a[condi]
+
+# condi를 이용해 a안에 3의 배수의 총 합을 구하세요
+sum(a[condi])
+
+
+# 메뉴별로 벡터를 생성해 판매량을 요일순으로 저장
+espresso <- c(4,5,3,6,5,4,7)
+americano <- c(63,68,64,68,72,89,94)
+cafelatte <- c(61,70,59,71,71,92,88)
+
+# 메뉴별 판매량에 각각의 가격을 곱하여 메뉴마다 요일별 매출액을 구합니다.
+# 그 결과를 새로운 변수에 저장
+sale.espresso <- espresso*2
+sale.americano <- americano*2.5
+sale.cafelatte <- cafelatte*3
+
+# 각 메뉴의 매출액을 더하여 요일별 매출액을 구하고 이것을 새로운 변수에 저장합니다.
+# 값의 이름은 요일로 설정합니다.
+sale.day <- sale.espresso + sale.americano + sale.cafelatte
+names(sale.day) <- c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+sale.day
+
+# 요일별 매출액을 합산하여 일주일간 총 매출액을 구합니다.
+sum(sale.day)
+
+# 요일별 매출액을 벡터로 일평균 매출액을 구하고 새로운 변수에 저장
+sale.mean <- mean(sale.day)
+
+# 각 요일의 매출액과 평균 매출액을 비교하여 평균 이상인 요일을 구한다.
+names(sale.day[sale.day >= sale.mean])
